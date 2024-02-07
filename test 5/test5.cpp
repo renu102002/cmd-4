6e564c4c4b6d814852d6778e3ce65f46599361bd
renu102002 <157440265+renu102002@users.noreply.github.com>
2024-02-07 01:30:35 -0500

    first commit

---
+++ test 5/test5.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+#include <iostream>
+using namespace std;
+int main()
+{
+
+	int mynumbers[5] = { 10,20,30,40,50 };
+	cout << sizeof(mynumbers) << endl;
+	cout << sizeof(int) << endl;
+}
